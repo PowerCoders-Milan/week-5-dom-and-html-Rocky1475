bd70e12fc1757f6cd270cd7024ba86d61dafac9e
Muhammad rRehman <muhammadislamian@gmail.com>
2021-03-02 15:11:12 +0000

    ex 5 started

--- ex-5/exercise.js
+++ ex-5/exercise.js
@@ -1,9 +1,14 @@
 function loadBooks(array) {
+    var (i - 0; i(array.length; i++){
+        var para =
+    }
 
 }
 
 // Paste here your array
-
+var books = [
+    { title: 'The Design of EveryDay Things', author: 'Don Norman', alreadyRead: false }, { title: 'The power of now', author: 'Ekhart Tolle', alreadyRead: true }, { title: 'The Three Body Problem', author: 'Liu Cixin', alreadyRead: true }
+];
 
 // This function will execute when the window loads
 window.onload = loadBooks(array);
