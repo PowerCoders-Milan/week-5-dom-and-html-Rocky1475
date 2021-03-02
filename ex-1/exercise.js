8fba3afbafbf2f3284a0af1e21589dacbd2ec56f
Muhammad rRehman <muhammadislamian@gmail.com>
2021-03-02 11:46:14 +0000

    ex 1 comepleted

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,5 +1,9 @@
 // Use this function that is linked to the button to change the background
 
 function set_background() {
+    document.body.style.backgroundColor = "pink";
+    document.getElementById("P1").style.backgroundColor = "lightblue"
+    document.getElementById("P2").style.backgroundColor = "red"
+    document.getElementById("P3").style.backgroundColor = "yellow"
 
 }
